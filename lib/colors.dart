e23a93d99e76a201dd755fe148f74cf4e2cf68d3
Orpheus Peng <stannolite@gmail.com>
2020-08-12 00:02:26 -0700

    Reset main.dart, included missing gitignore file

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class PolarmorphColor {
+  static final black = Color(0xFF404040);
+  static final red = Color(0xFFFF5353);
+  static final white = Color(0xFFFCFCFC);
+}
