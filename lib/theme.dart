c525eb940724645126569728f4705b4d1b3f91bb
Orpheus Peng <stannolite@gmail.com>
2020-08-12 16:52:38 -0700

    TextStyle.height actually specifies the line height as a **multiple** of font size, instead of a numerical value in pixel

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,91 +1,109 @@
 import 'package:flutter/material.dart';
 import 'colors.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 final polarmorphTextTheme = TextTheme(
-  headline1: GoogleFonts.cabinCondensed(
+  headline1: TextStyle(
+    fontFamily: 'Cabin Condensed',
     fontSize: 96,
     fontWeight: FontWeight.w700,
     letterSpacing: 0,
     color: PolarmorphColor.black,
-    height: 96,
+    height: 1,
   ),
-  headline2: GoogleFonts.cabinCondensed(
+  headline2: TextStyle(
+    fontFamily: 'Cabin Condensed',
     fontSize: 60,
     fontWeight: FontWeight.w700,
     letterSpacing: 0,
     color: PolarmorphColor.black,
-    height: 60,
+    height: 1,
   ),
-  headline3: GoogleFonts.arvo(
+  headline3: TextStyle(
+    fontFamily: 'Arvo',
     fontSize: 48,
     fontWeight: FontWeight.w400,
     letterSpacing: 0,
     color: PolarmorphColor.black,
-    height: 48,
+    height: 1,
   ),
-  headline4: GoogleFonts.arvo(
+  headline4: TextStyle(
+    fontFamily: 'Arvo',
     fontSize: 34,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.25,
     color: PolarmorphColor.black,
-    height: 34,
+    height: 1,
   ),
-  headline5: GoogleFonts.cabin(
+  headline5: TextStyle(
+    fontFamily: 'Cabin',
     fontSize: 24,
     fontWeight: FontWeight.w700,
     letterSpacing: 0.15,
     color: PolarmorphColor.black,
-    height: 24,
+    height: 1,
   ),
-  headline6: GoogleFonts.cabin(
+  headline6: TextStyle(
+    fontFamily: 'Cabin',
     fontSize: 20,
     fontWeight: FontWeight.w700,
     letterSpacing: 0,
     color: PolarmorphColor.black,
-    height: 20,
+    height: 1,
   ),
-  subtitle1: GoogleFonts.cabin(
+  subtitle1: TextStyle(
+    fontFamily: 'Cabin',
     fontSize: 20,
     fontWeight: FontWeight.w700,
     letterSpacing: 0.15,
     color: PolarmorphColor.black,
+    height: 1.5,
   ),
-  subtitle2: GoogleFonts.cabin(
+  subtitle2: TextStyle(
+    fontFamily: 'Cabin',
     fontSize: 16,
     fontWeight: FontWeight.w700,
     letterSpacing: 0.1,
     color: PolarmorphColor.black,
+    height: 1.5,
   ),
-  bodyText1: GoogleFonts.arvo(
+  bodyText1: TextStyle(
+    fontFamily: 'Arvo',
     fontSize: 20,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.5,
     color: PolarmorphColor.black,
+    height: 1.2,
   ),
-  bodyText2: GoogleFonts.arvo(
+  bodyText2: TextStyle(
+    fontFamily: 'Arvo',
     fontSize: 16,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.25,
     color: PolarmorphColor.black,
   ),
-  button: GoogleFonts.cabin(
+  button: TextStyle(
+    fontFamily: 'Cabin',
     fontSize: 14,
     fontWeight: FontWeight.w400,
     letterSpacing: 1.25,
     color: PolarmorphColor.black,
+    height: 1.2,
   ),
-  caption: GoogleFonts.cabin(
+  caption: TextStyle(
+    fontFamily: 'Cabin',
     fontSize: 12,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.4,
     color: PolarmorphColor.black,
+    height: 1.2,
   ),
-  overline: GoogleFonts.cabin(
+  overline: TextStyle(
+    fontFamily: 'Cabin',
     fontSize: 10,
     fontWeight: FontWeight.w400,
     letterSpacing: 1.5,
     color: PolarmorphColor.red,
+    height: 1,
   ),
 );
 
