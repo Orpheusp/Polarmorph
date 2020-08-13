8c4a926281caec1e2f4867bf594fcec12c3ed4b3
Orpheus Peng <stannolite@gmail.com>
2020-08-12 18:27:57 -0700

    Clean up colors.dart

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PolarmorphColor {
-  static final black = Color(0xFF404040);
-  static final red = Color(0xFFFF5353);
-  static final white = Color(0xFFFCFCFC);
+  static const black = Color(0xFF404040);
+  static const red = Color(0xFFFF5353);
+  static const white = Color(0xFFFCFCFC);
 }
