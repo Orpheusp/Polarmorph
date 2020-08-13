2f61e7e4828c281ed30ae837a2f97c64812bfb2e
Orpheus Peng <stannolite@gmail.com>
2020-08-12 18:28:22 -0700

    more cleanups

--- lib/business_hours.dart
+++ lib/business_hours.dart
@@ -9,24 +9,20 @@ class BusinessHours extends StatelessWidget {
 
   Widget getBusinessHoursRow(BuildContext context, String begin, String end) {
     final textStyle = Theme.of(context).textTheme.headline3;
-    final row = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text(begin, style: textStyle),
-          Expanded(
-            child: Divider(
-              color: PolarmorphColor.black,
-              height: textStyle.height * textStyle.fontSize,
-              thickness: 2,
-              indent: 10,
-              endIndent: 10,
-            ),
+    final row = Row(
+      children: <Widget>[
+        Text(begin, style: textStyle),
+        Expanded(
+          child: Divider(
+            color: PolarmorphColor.black,
+            height: textStyle.height * textStyle.fontSize,
+            thickness: 2,
+            indent: 10,
+            endIndent: 10,
           ),
-          Text(end, style: textStyle),
-        ],
-      ),
+        ),
+        Text(end, style: textStyle),
+      ],
     );
 
     return row;
@@ -35,7 +31,6 @@ class BusinessHours extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         this.getBusinessHoursRow(context, START_HOUR, END_HOUR),
         this.getBusinessHoursRow(context, START_DAY, END_DAY),
